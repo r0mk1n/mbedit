3ffe2c962acb00d9882d8a1731a669dce194cb09
r0mk1n <r0mk1n.sti@gmail.com>
2016-07-05 22:17:20 +0300

    * updated palette buttons styles * added level editor canvas / event handlers * added palette color selecting

--- public/js/views/LevelEditView.js
+++ public/js/views/LevelEditView.js
@@ -10,6 +10,8 @@ var LevelEditView = Backbone.View.extend({
      */
     app                     : null,
 
+    selected_palette_class  : 'selected',
+
     /**
      * Handler for level modal
      */
@@ -27,11 +29,25 @@ var LevelEditView = Backbone.View.extend({
      */
     callback                : null,
 
+    $levelCursorPosition    : null,
+    $canvas                 : null,
+    ctx                     : null,
+
+    cw                      : 0,
+    ch                      : 0,
+
+    cellXSize               : 0,
+    cellYSize               : 0,
+
+    selected_color          : 0,
+
     /**
      * Override from Backbone.View
      */
     initialize: function( options ){
         this.app = options.app;
+
+        var self = this;
 
         // create modal
         this.$el.append( _.template( this.templates.modal ) );
@@ -43,6 +59,31 @@ var LevelEditView = Backbone.View.extend({
         for ( var i = 0; i < 10; i++ ) {
             $palette_placehodler.append( _.template( this.templates.palette_btn )( {index: i} ) );
         }
+
+        this.$levelCursorPosition = $('#level-cursor-position');
+
+        this.$canvas = $('#level-data');
+        this.ctx = this.$canvas[0].getContext("2d");
+
+        this.cw = this.ctx.canvas.width;
+        this.ch = this.ctx.canvas.height;
+
+        this.cellXSize = this.cw / 10;
+        this.cellYSize = this.ch / 10;
+
+        $('.color-item').on( 'click', function( event ) {
+            self.__selectColor( $(this).attr('data-color-id') );
+        });
+
+        // add event listeners
+        this.$canvas.on( 'mousemove', function( event ) {
+            self.__updateCursorPosition( event.offsetX, event.offsetY );
+        });
+
+        this.$canvas.on( 'click', function( event ) {
+
+        });
+
     },
 
     /**
@@ -55,7 +96,6 @@ var LevelEditView = Backbone.View.extend({
         this.callback = callback;
 
         var category_data = this.app.getCategoryInfoById( data.category_id );
-
         var categoris_list = this.app.getCategoriesList();
 
         if ( this.data.hasOwnProperty( '_id' ) ) {
@@ -68,9 +108,9 @@ var LevelEditView = Backbone.View.extend({
 
         this.$levelModalTitle.html( this.$levelModalTitle.html() + '<strong> ' + category_data.attributes.name + '</strong>' );
 
+        this.__selectColor(0);
 
         this.$levelModal.modal('show');
-
     },
 
     /**
@@ -80,6 +120,22 @@ var LevelEditView = Backbone.View.extend({
 
     },
 
+    // private /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    __selectColor: function( color_index ) {
+        this.selected_color = color_index;
+        $('.color-item').removeClass( this.selected_palette_class );
+        $('#palette-item-' + this.selected_color).addClass( this.selected_palette_class );
+    },
+
+    __updateCursorPosition: function( x, y ) {
+        this.$levelCursorPosition.html( '[ ' + this.__pad( Math.round( x / this.cellXSize ) ) + ' - ' + this.__pad( Math.round( y / this.cellYSize ) ) + ' ]' );
+    },
+
+
+    __pad: function( n ) {
+        return (n < 10) ? ("0" + n) : n;
+    },
     // events handlers /////////////////////////////////////////////////////////////////////////////////////////////////
 
     /**
@@ -100,11 +156,14 @@ var LevelEditView = Backbone.View.extend({
                     '</div>' +
                     '<div class="modal-body">' +
                         '<div class="row">' +
-                            '<div class="col-md-9">' +
+                            '<div class="col-md-11">' +
                                 '<div class="level-toolbar"></div>'+
-                                '<div class="level-placeholder"  id="level-placeholder"></div>' +
+                                '<div class="level-placeholder"  id="level-placeholder" >' +
+                                    '<canvas id="level-data" class="effect7" width="400" height="400"></canvas>' +
+                                    '<h5 id="level-cursor-position">[ 00 - 00 ]</h5>' +
+                                '</div>' +
                             '</div>'+
-                            '<div class="col-md-3" id="palette-placeholder"></div>'+
+                            '<div class="col-md-1" id="palette-placeholder"></div>'+
                         '</div>'+
                     '</div>' +
                     '<div class="modal-footer">' +
@@ -116,7 +175,7 @@ var LevelEditView = Backbone.View.extend({
         '</div>' +
         '',
         palette_btn: '' +
-            '<a id="palette-item-<%= index %>" class="btn btn-block btn-default color-item color-<%= index %>" data-ccolor-id="<%= index %>">&nbsp;</a>' +
+            '<a id="palette-item-<%= index %>" class="color-item color-<%= index %> " data-color-id="<%= index %>"></a>' +
         ''
     }
 });
